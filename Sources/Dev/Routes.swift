9901c8520955777f6f3f86ab95a0d2e48a3686d6
John Connolly <connoljo2@gmail.com>
2018-06-27 22:55:46 -0400

    Test

--- Sources/Dev/Routes.swift
+++ Sources/Dev/Routes.swift
@@ -16,9 +16,9 @@ struct Car: Codable {
 let utf8String = .utf8 |> flip(curry(String.init(data:encoding:)))
 
 let siteMap = [
-    curry(users) <^> (path("users") *> string) <*> int |> get,
-    cars <^> path("cars") *> int |> get,
-    hello |> pure <*> path("hello") *> end |> get
+    curry(users) <^> (path("users") *> string) <*> int |> get
+//    cars <^> path("cars") *> int |> get,
+//    hello |> pure <*> path("hello") *> end |> get
 ]
 
 func hello() -> (Request) -> AnyResponse {
