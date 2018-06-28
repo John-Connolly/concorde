a7dc3cb7b6dcb42f4ba2dcb44a111dfa8df14329
John Connolly <connoljo2@gmail.com>
2018-06-27 22:54:30 -0400

    Trying to compile on linux

--- Sources/Dev/Routes.swift
+++ Sources/Dev/Routes.swift
@@ -16,7 +16,7 @@ struct Car: Codable {
 let utf8String = .utf8 |> flip(curry(String.init(data:encoding:)))
 
 let siteMap = [
-    users |> curry <^> (path("users") *> string) <*> int |> get,
+    curry(users) <^> (path("users") *> string) <*> int |> get,
     cars <^> path("cars") *> int |> get,
     hello |> pure <*> path("hello") *> end |> get
 ]
@@ -41,27 +41,10 @@ func cars(amount: Int) -> (Request) -> AnyResponse {
     }
 }
 
-
-
-
-//let post = route(method: .POST)
-
-//let hello = get("/hello") { req -> AnyResponse in
-//    return AnyResponse(item: "hello word")
-//}
-//
-//let vehicle = get("/car") { req -> AnyResponse in
-//    return cars |> AnyResponse.init(item:)
-//}
-//
 //let token = get("/loaderio-95e2de71ba5cfa095645d825903bc632/") { req -> AnyResponse in
 //    return "loaderio-95e2de71ba5cfa095645d825903bc632" |> AnyResponse.init(item:)
 //}
-//
-//let largerResp = get("/large") { req -> AnyResponse in
-//    return AnyResponse(item: cars + cars + cars + cars + cars + cars)
-//}
-//
+
 //let update = post("/update") { req -> AnyResponse in
 //    return (req.body >>- utf8String <^> AnyResponse.init(item:)) ?? .error
 //}
