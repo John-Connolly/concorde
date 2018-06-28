c16eb8bdde76601ad5127af4e70d57e5a4a3e033
John Connolly <connoljo2@gmail.com>
2018-06-27 23:04:01 -0400

    Trying again

--- Sources/Dev/Routes.swift
+++ Sources/Dev/Routes.swift
@@ -17,7 +17,7 @@ let utf8String = .utf8 |> flip(curry(String.init(data:encoding:)))
 
 let siteMap = [
     curry(users) <^> (path("users") *> string) <*> int |> get,
-    cars <^> path("cars") *> int |> get
+    cars <^> (path("cars") *> int) |> get
 //    hello |> pure <*> path("hello") *> end |> get
 ]
 
