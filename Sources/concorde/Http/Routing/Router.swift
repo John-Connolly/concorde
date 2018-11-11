6c998da4f85c0d49aa2143e2e8cf08303dd789ac
John Connolly <connoljo2@gmail.com>
2018-11-10 23:24:36 -0400

    Formatting

--- Sources/concorde/Http/Routing/Router.swift
+++ Sources/concorde/Http/Routing/Router.swift
@@ -12,7 +12,10 @@ import NIOHTTP1
 public let get = register(method: .GET)
 public let post = register(method: .POST)
 
-public func router(register routes: [(Request) -> (Future<Response>?)]) -> (Request, (Future<Response>) -> ()) -> () {
+public func router(register routes: [(Request) -> (Future<Response>?)])
+    -> (Request, (Future<Response>)
+    -> ())
+    -> () {
     return { request, responder in
         for route in routes {
             guard let resp = route(request) else {
@@ -25,19 +28,20 @@ public func router(register routes: [(Request) -> (Future<Response>?)]) -> (Requ
     }
 }
 
-public func register(method: HTTPMethod) -> (Route<(Request) -> Future<Response>>) -> (Request) -> Future<Response>? {
-    return { route in
-        return { req in
-            guard req.method == method else {
-                return nil
-            }
+public func register(method: HTTPMethod)
+    -> (Route<(Request) -> Future<Response>>)
+    -> (Request)
+    -> Future<Response>? {
+        return { route in
+            return { req in
+                guard req.method == method else {
+                    return nil
+                }
 
-            guard let matchedRoute = route.run(req.head.uri)?.0 else {
-                return nil
+                guard let matchedRoute = route.run(req.head.uri)?.0 else {
+                    return nil
+                }
+                return matchedRoute(req)
             }
-            return matchedRoute(req)
         }
-    }
 }
-
-
