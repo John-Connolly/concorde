8341d2473b1f1b04aa7a1e6307b7ad760eb3728e
John Connolly <connoljo2@gmail.com>
2019-03-09 20:37:19 -0400

    Changes to Routing

--- Sources/Dev/Failed.swift
+++ Sources/Dev/Failed.swift
@@ -10,41 +10,39 @@ import concorde
 import Html
 
 private let title = View<String, [Node]> { content in
-    return [
-        br,
-        h3([classAtr("h2")], [.text(content)]),
-        br,
-    ]
+  return [br, h3([classAtr("h2")], [.text(content)]), br]
 }
 
 private let tableComponent = View<MainViewContent, [Node]> { content in
-    return [
-        card(with: table(header: content.tableContent.tableHeader,
-                         rows: content.tableContent.content))
-    ]
+  return [card(with: table(
+    header: content.tableContent.tableHeader,
+    rows: content.tableContent.content
+  ))]
 }
-
-
 
 struct MainViewContent {
-    let tableContent: TableContent
+  let tableContent: TableContent
 
-    struct TableContent {
-        let tableHeader: [String]
-        let content: [[String]]
-    }
+  struct TableContent {
+    let tableHeader: [String]
+    let content: [[String]]
+  }
 
 }
 
-private let content = MainViewContent(tableContent: .init(tableHeader: ["#", "Worker", "Failed Count"], content: [["Worker 1", "hello"]]))
+private let content = MainViewContent(
+  tableContent: .init(
+    tableHeader: ["#", "Worker", "Failed Count"],
+    content: [["Worker 1", "hello"]]
+  )
+)
 
 private let failedV = (title.contramap { _ in "Failed" }
-    <> tableComponent
-    <> footerView.contramap { _ in })
-    .map(flip(curry(baseView))(.failed) >>> pure)
+  <> tableComponent
+  <> footerView.contramap { _ in }).map(flip(curry(baseView))(.failed) >>> pure)
 
 func failedView() -> String {
-    return render(failedV.view(content))
+  return render(failedV.view(content))
 }
 
 /// Include job types!
--- Sources/concorde/Http/Routing/Route.swift
+++ Sources/concorde/Http/Routing/Route.swift
@@ -8,35 +8,96 @@
 import Foundation
 import NIOHTTP1
 
+public indirect enum Endpoint {
+    case empty
+    case constant(String)
+    case parameter(String)
+    case joined(Endpoint, Endpoint)
+
+    public var pretty: String {
+        switch self {
+        case .empty:
+            return "/"
+        case .parameter(let param):
+            return ":" + param
+        case .constant(let str):
+            return str
+        case .joined(let a, let b):
+            return a.pretty + b.pretty
+        }
+    }
+}
+
 public struct Route<A> {
     public typealias Stream = ArraySlice<String>
     public let parse: (Stream) -> (A, Stream)?
-    var inverse: ((A) -> String)? = nil
+    public let inverse: () -> Endpoint?
     public let method: HTTPMethod
-    /// String representation of what is being matched on.
-    let uriFormat: String
+
+//    public func prettyPrint(_ x: A) -> String? {
+//        return inverse(x)
+//    }
 }
 
 extension Route {
 
     init(parse: @escaping (Stream) -> (A, Stream)?) {
         self.parse = parse
-        self.uriFormat = "/"
         self.method = .GET // FIX
+        self.inverse = {
+            return .empty
+        }
     }
 
-    init(uriFormat: String, parse: @escaping (Stream) -> (A, Stream)?) {
+    init(_ parse: @escaping (Stream) -> (A, Stream)?, inverse: @escaping () -> Endpoint?) {
         self.parse = parse
-        self.uriFormat = uriFormat
-        self.method = .GET
+        self.method = .GET // FIX
+        self.inverse = inverse
     }
+//
+//    init(_ parse: @escaping (Stream) -> (A, Stream)?) {
+//        self.parse = parse
+//        self.method = .GET // FIX
+//        self.inverse =  {
+//            return .parameter(String(describing: A.self))
+//        }
+//    }
 
-    init(method: HTTPMethod, uriFormat: String, parse: @escaping (Stream) -> (A, Stream)?) {
+
+    init(method: HTTPMethod, parse: @escaping (Stream) -> (A, Stream)?) {
         self.parse = parse
         self.method = method
-        self.uriFormat = uriFormat
+        self.inverse = { 
+            return .empty
+        }
     }
 }
+
+
+extension Route where A: ExpressibleByIntegerLiteral {
+
+    init(_ parse: @escaping (Stream) -> (A, Stream)?) {
+        self.parse = parse
+        self.method = .GET // FIX
+        self.inverse =  {
+            return .parameter(String(describing: A.self))
+        }
+    }
+
+}
+
+//extension Route where A == String {
+//
+//    init(uriFormat: String, parse: @escaping (Stream) -> (A, Stream)?) {
+//        self.parse = parse
+//        self.uriFormat = uriFormat
+//        self.method = .GET
+//        self.inverse = { in
+//            return a
+//        }
+//    }
+//
+//}
 
 extension Route {
 
@@ -50,18 +111,34 @@ extension Route {
         return parse(combined.dropFirst())
     }
 
+
+//    func transform<B>(_ to: @escaping (A) -> B, _ from: @escaping (B) -> A?) -> Route<B> {
+//        return Route<B>(parse: { input in
+//            guard let (result, remainder) = self.parse(input) else { return nil }
+//            return (to(result), remainder)
+//        }, inverse: { value in
+//            return from(value).flatMap(self.inverse)
+//        })
+//    }
+
+
     public func map<T>(_ transform: @escaping (A) -> T) -> Route<T> {
-        return Route<T>(uriFormat: uriFormat) { input in
+        return Route<T>.init({ input in
             guard let (result, remainder) = self.parse(input) else { return nil }
             return (transform(result), remainder)
-        }
+        }, inverse: {
+            return self.inverse()
+        })
     }
 
     public func followed<B>(by other: Route<B>) -> Route<(A, B)> {
-        return Route<(A, B)>(uriFormat: uriFormat + "/" + other.uriFormat) { input in
+        return Route<(A,B)>.init( { input -> ((A, B), ArraySlice<String>)? in
             guard let (result1, remainder1) = self.parse(input) else { return nil }
             guard let (result2, remainder2) = other.parse(remainder1) else { return nil }
             return ((result1, result2), remainder2)
+        }) {
+            guard let a = self.inverse(), let b = other.inverse() else { return nil }
+            return .joined(a, b)
         }
     }
 
@@ -72,23 +149,18 @@ extension Route {
     }
 }
 
+
 public func choice<A>(_ routes: [Route<A>]) -> Route<A> {
     return routes.dropFirst().reduce(routes[0], { $0.or($1) })
 }
 
-public func prettyPrint(_ route: Route<(Conn) -> Future<Conn>>) -> String {
-    return route.uriFormat + "\n"
-}
-
-public func prettyPrint(_ routes: [Route<(Conn) -> Future<Conn>>]) -> [String] {
-    return routes.map(prettyPrint)
-}
-
 public func path(_ matching: String) -> Route<String> {
-    return Route(uriFormat: "/" + matching) { input in
+    return Route({ input -> (String, ArraySlice<String>)? in
         guard let path = input.first, path == matching else { return nil }
         return (path, input.dropFirst())
-    }
+    }, inverse: {
+        return .constant(matching)
+    })
 }
 
 public let end: Route<()> = Route { input in
@@ -97,7 +169,7 @@ public let end: Route<()> = Route { input in
 }
 
 public func method<A>(_ method: HTTPMethod, route: Route<A>) -> Route<A> {
-    return Route(method: method, uriFormat: route.uriFormat, parse: route.parse)
+    return Route(method: method, parse: route.parse)
 }
 
 public let suffix: Route<String> = Route { input in
@@ -105,85 +177,79 @@ public let suffix: Route<String> = Route { input in
     return (last, input)
 }
 
-public let double: Route<Double> = Route(uriFormat: ":Double") { input in
+public let double: Route<Double> = Route { input in
     guard let double = input.first.flatMap(Double.init) else { return nil }
     return (double, input.dropFirst())
 }
 
-public let string: Route<String> = Route(uriFormat: ":String") { input in
+public let string: Route<String> = Route({ input in
     guard let string = input.first else { return nil }
     return (string, input.dropFirst())
-}
+}, inverse: {
+    return .parameter("String")
+})
 
-public let uuid: Route<UUID> = Route(uriFormat: ":UUID") { input in
+public let uuid: Route<UUID> = Route { input in
     guard let string = input.first, let uuid = UUID.init(uuidString: string) else { return nil }
     return (uuid, input.dropFirst())
 }
 
-public let int: Route<Int> = Route(uriFormat: ":Int") { input in
+public let int: Route<Int> = Route { input in
     guard let int = input.first.flatMap(Int.init) else { return nil }
     return (int, input.dropFirst())
 }
 
-public let int8: Route<Int8> = Route(uriFormat: ":Int8") { input in
+public let int8: Route<Int8> = Route { input in
     guard let int = input.first.flatMap(Int8.init) else { return nil }
     return (int, input.dropFirst())
 }
 
-public let int16: Route<Int16> = Route(uriFormat: ":Int16") { input in
+public let int16: Route<Int16> = Route { input in
     guard let int = input.first.flatMap(Int16.init) else { return nil }
     return (int, input.dropFirst())
 }
 
-public let int32: Route<Int32> = Route(uriFormat: ":Int32") { input in
+public let int32: Route<Int32> = Route { input in
     guard let int = input.first.flatMap(Int32.init) else { return nil }
     return (int, input.dropFirst())
 }
 
-public let int64: Route<Int64> = Route(uriFormat: ":Int64") { input in
+public let int64: Route<Int64> = Route { input in
     guard let int = input.first.flatMap(Int64.init) else { return nil }
     return (int, input.dropFirst())
 }
 
-public let UInt: Route<UInt> = Route(uriFormat: ":UInt") { input in
+public let UInt: Route<UInt> = Route { input in
     guard let uint = input.first.flatMap(Swift.UInt.init) else { return nil }
     return (uint, input.dropFirst())
 }
 
-public let UInt8: Route<UInt8> = Route(uriFormat: ":UInt8") { input in
+public let UInt8: Route<UInt8> = Route { input in
     guard let uint = input.first.flatMap(Swift.UInt8.init) else { return nil }
     return (uint, input.dropFirst())
 }
 
-public let UInt16: Route<UInt16> = Route(uriFormat: ":UInt16") { input in
+public let UInt16: Route<UInt16> = Route { input in
     guard let uint = input.first.flatMap(Swift.UInt16.init) else { return nil }
     return (uint, input.dropFirst())
 }
 
-public let UInt32: Route<UInt32> = Route(uriFormat: ":UInt32") { input in
+public let UInt32: Route<UInt32> = Route { input in
     guard let uint = input.first.flatMap(Swift.UInt32.init) else { return nil }
     return (uint, input.dropFirst())
 }
 
-public let UInt64: Route<UInt64> = Route(uriFormat: ":UInt64") { input in
+public let UInt64: Route<UInt64> = Route { input in
     guard let uint = input.first.flatMap(Swift.UInt64.init) else { return nil }
     return (uint, input.dropFirst())
 }
 
 public func query(_ param: String) -> Route<String> {
-    return Route(uriFormat: "/" + "?\(param)=") { input in
+    return Route { input in
         let components = input.first?.split(separator: "=")
         guard let key = components?.first,
               let value = components?.last,
               key == param else { return nil }
         return (String(value), input.dropFirst())
     }
-}
-
-
-enum ComponentType {
-    case string
-    case double
-    indirect case bind(String, to: ComponentType, in: ComponentType)
-
 }
