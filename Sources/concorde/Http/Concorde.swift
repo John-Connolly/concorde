339605979922a80daac698aeb92ba83062f28dc7
John Connolly <connoljo2@gmail.com>
2019-04-03 13:48:58 -0300

    Test

--- Sources/concorde/Http/Concorde.swift
+++ Sources/concorde/Http/Concorde.swift
@@ -8,75 +8,15 @@ let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount) //Sys
 private func create(
     router: @escaping (Conn, (EventLoopFuture<Conn>) -> ()) -> (),
     config: Configuration
-    ) -> ServerBootstrap {
-
-    let variable = ThreadSpecificVariable<ThreadCache>()
-
-    for _ in 0..<System.coreCount {
-        let loop = group.next()
-
-        loop.submit {
-            var cons = config.resources.map { $0(loop) }
-            let threadPool = NIOThreadPool(numberOfThreads: 1)
-            threadPool.start()
-            cons.append(threadPool as Any)
-            variable.currentValue = ThreadCache(items: cons)
-        }.whenFailure { error in
-                fatalError("Could not boot eventloop: \(error)")
-        }
-    }
-
-    // Specify backlog and enable SO_REUSEADDR for the server itself
-    let bootstrap = ServerBootstrap(group: group)
-        .serverChannelOption(ChannelOptions.backlog, value: 256)
-        .serverChannelOption(
-            ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR),
-            value: 1
-        )
-
-        .childChannelInitializer { channel in
-            // Ensure we don't read faster then we can write by adding the BackPressureHandler into the pipeline.
-            channel
-                .pipeline
-                .configureHTTPServerPipeline(withPipeliningAssistance: true)
-                .flatMap { _ in
-                    channel.pipeline
-                        .addHandler(BackPressureHandler())
-                        .flatMap { _ in
-                            channel.pipeline.addHandler(HTTPHandler(
-                                with: router,
-                                and: variable
-                            ))
-                    }
-            }
-        }
-
-        // Enable TCP_NODELAY and SO_REUSEADDR for the accepted Channels
-        .childChannelOption(
-            ChannelOptions.socket(IPPROTO_TCP, TCP_NODELAY),
-            value: 1
-        )
-        .childChannelOption(
-            ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR),
-            value: 1
-        )
-        .childChannelOption(ChannelOptions.maxMessagesPerRead, value: 1)
-        .childChannelOption(
-            ChannelOptions.recvAllocator,
-            value: AdaptiveRecvByteBufferAllocator()
-    )
-    return bootstrap
+    ) -> String {
+    return ""
 }
 //
 //// Fix this!!
-private func start(_ bootstrap: ServerBootstrap) -> Reader<Configuration,
+private func start(_ bootstrap: String) -> Reader<Configuration,
     Never> {
         return Reader<Configuration, Never> { config in
-            print("Listening on localhost:\(config.port)")
-            let channel = try! bootstrap
-                .bind(host: "localhost", port: config.port)
-                .wait()
-            try! channel.closeFuture.wait()
+
             exit(0)
         }
 }
