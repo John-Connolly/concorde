db09d0025157f152aab74e410b0e4a17f3586159
John Connolly <connoljo2@gmail.com>
2019-04-03 13:51:45 -0300

    Test

--- Sources/concorde/Http/Concorde.swift
+++ Sources/concorde/Http/Concorde.swift
@@ -4,15 +4,15 @@ import NIOHTTP1
 
 public let concorde = create >>> start
 
-let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount) //System.coreCount
+
 private func create(
-    router: @escaping (Conn, (EventLoopFuture<Conn>) -> ()) -> (),
-    config: Configuration
+    router: @escaping (Conn, (EventLoopFuture<Conn>) -> ()) -> ()
     ) -> String {
+
     return ""
 }
 //
 //// Fix this!!
 private func start(_ bootstrap: String) {
-    
+
 }
