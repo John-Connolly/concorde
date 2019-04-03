d6a07335e6af638bf4c1be670e51d33d3c2a0eaf
John Connolly <connoljo2@gmail.com>
2019-04-03 14:23:28 -0300

    Test

--- Sources/concorde/Http/Concorde.swift
+++ Sources/concorde/Http/Concorde.swift
@@ -2,13 +2,18 @@ import Foundation
 import NIO
 import NIOHTTP1
 
-public let fandg = f >>> g
+//public let fandg = f >>> g
 
-
-private func f(h: @escaping () -> (), other: String) -> String {
-    return ""
-}
-
-private func g(_ str: String) {
+func nothing() {
 
 }
+
+let thing = (nothing, "")
+
+//private func f(h: @escaping () -> (), other: String) -> String {
+//    return ""
+//}
+//
+//private func g(_ str: String) {
+//
+//}
