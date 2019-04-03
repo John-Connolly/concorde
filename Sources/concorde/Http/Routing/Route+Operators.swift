0da9485b66ecf425820255b949b2a02570decaed
John Connolly <connoljo2@gmail.com>
2019-04-03 13:43:21 -0300

    Test

--- Sources/concorde/Http/Routing/Route+Operators.swift
+++ Sources/concorde/Http/Routing/Route+Operators.swift
@@ -7,27 +7,27 @@
 
 import Foundation
 
-infix operator <*>: ParserPrecedence
-infix operator <^>: ParserPrecedence
-infix operator *>: ParserPrecedence
-
-public func *> <A, B>(lhs: Route<A>, rhs: Route<B>) -> Route<B> {
-    return const(id) <^> lhs <*> rhs
-}
-
-/// Functor
-public func <^> <A, B>(lhs: @escaping (A) -> B, rhs: Route<A>) -> Route<B> {
-    return rhs.map(lhs)
-}
-
-/// Applicative
-public func <*> <A, B>(lhs: Route<(A) -> B>, rhs: Route<A>) -> Route<B> {
-    return lhs.followed(by: rhs).map { f, x in f(x) }
-}
-
-
-public func pure<A>(_ a: A) -> Route<A> {
-    return Route { stream in
-        return (a, stream)
-    }
-}
+//infix operator <*>: ParserPrecedence
+//infix operator <^>: ParserPrecedence
+//infix operator *>: ParserPrecedence
+//
+//public func *> <A, B>(lhs: Route<A>, rhs: Route<B>) -> Route<B> {
+//    return const(id) <^> lhs <*> rhs
+//}
+//
+///// Functor
+//public func <^> <A, B>(lhs: @escaping (A) -> B, rhs: Route<A>) -> Route<B> {
+//    return rhs.map(lhs)
+//}
+//
+///// Applicative
+//public func <*> <A, B>(lhs: Route<(A) -> B>, rhs: Route<A>) -> Route<B> {
+//    return lhs.followed(by: rhs).map { f, x in f(x) }
+//}
+//
+//
+//public func pure<A>(_ a: A) -> Route<A> {
+//    return Route { stream in
+//        return (a, stream)
+//    }
+//}
